09394255e3ee646358c693f3b0fc6c7e74a3b9b5
maurizio100 <mauriziorinder@gmx.at>
2012-04-10 14:00:19 +0200

    Adding the client to project

---
+++ client.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<string.h>
+#include<unistd.h>
+
