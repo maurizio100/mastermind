2ff91a95164c20aec87734b21b40bc783c2804f8
maurizio100 <mauriziorinder@gmx.at>
2012-04-10 14:04:44 +0200

    *** empty log message ***

--- client.c
+++ client.c
@@ -8,3 +8,22 @@
 #include<string.h>
 #include<unistd.h>
 
+
+static int open_client_socket(char* const server_hostname, const char* server_service){
+  int sockfd = -1;
+  struct addrinfo* ai,* ai_sel = NULL,* ai_head;
+  struct addrinfo hints;
+  int err;
+
+  /* 1) resolve hostname to IPV4 address*/
+  hints.ai_flags = 0;
+  hints.ai_family = AF_INET; /*ipv4*/
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+  hints.ai_addrlen = 0;
+  hints.ai_addr = NULL;
+  hints.ai_canonname = NULL;
+  hints.ai_next = NULL:
+
+
+}
